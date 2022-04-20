7edd95718f4af5c894d445b83c2fbd78d88ba90b
Claire Novotny <claire@novotny.org>
2022-04-20 17:41:51 -0500

    add iniital version

---
+++ MultiTarget/TheClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiTarget
+{
+    public class TheClass
+    {
+#if NET6_0_OR_GREATER
+    public void DoStringManipulation(ReadOnlySpan<char> input)
+    {
+        // use spans to do string operations.
+    }
+#else
+        public void DoStringManipulation(string input)
+        {
+            // Do some string operations.
+        }
+#endif
+    }
+}
